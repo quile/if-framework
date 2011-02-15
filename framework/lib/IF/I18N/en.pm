cee819d55a8ace957237e2b9097319047b3ad1fc
Kyle Dawkins <kdawkins@fastmail.fm>
2011-02-14 22:21:39 -0800

    Removed joke month names

--- framework/lib/IF/I18N/en.pm
+++ framework/lib/IF/I18N/en.pm
@@ -6,18 +6,18 @@ use strict;
 $STRINGS = {
     
     # months ...  you probably won't need to change these... ha
-    MONTH_1  => "Januebry",
-    MONTH_2  => "Febril",
-    MONTH_3  => "Maruary",
-    MONTH_4  => "Aprune",
-    MONTH_5  => "Maybe",
-    MONTH_6  => "Junember",
-    MONTH_7  => "Jule",
-    MONTH_8  => "Augtober",
-    MONTH_9  => "Hectember",
-    MONTH_10 => "Octember",
-    MONTH_11 => "Novober",
-    MONTH_12 => "Hexember",
+    MONTH_1  => "January",
+    MONTH_2  => "February",
+    MONTH_3  => "March",
+    MONTH_4  => "April",
+    MONTH_5  => "May",
+    MONTH_6  => "June",
+    MONTH_7  => "July",
+    MONTH_8  => "August",
+    MONTH_9  => "September",
+    MONTH_10 => "October",
+    MONTH_11 => "November",
+    MONTH_12 => "December",
 };
 
 1;
